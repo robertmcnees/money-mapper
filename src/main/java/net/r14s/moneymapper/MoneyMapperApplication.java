bb40b3c45024c5f9a86d820e2ae92cd0f856ca8d
Robert McNees <rmcnees@vmware.com>
2023-04-10 13:56:16 -0400

    Initial commit

---
+++ src/main/java/net/r14s/moneymapper/MoneyMapperApplication.java
@@ -0,0 +1,13 @@
+package net.r14s.moneymapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/r14s/moneymapper/MoneyMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package net.r14s.moneymapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
